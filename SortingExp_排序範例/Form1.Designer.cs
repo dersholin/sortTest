8b6b2caba4866b024ca8f3fe61d916a3af79c574
DerShoLin <65223957+dersholin@users.noreply.github.com>
2020-12-27 05:34:40 +0800

    Add files via upload

---
+++ SortingExp_排序範例/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace SortingExp_排序範例
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.TxQNo = new System.Windows.Forms.TextBox();
+            this.TxRRg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnBubble = new System.Windows.Forms.Button();
+            this.btnTree = new System.Windows.Forms.Button();
+            this.btnRadix = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.btnQuick = new System.Windows.Forms.Button();
+            this.btnShell = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxQNo
+            // 
+            this.TxQNo.Location = new System.Drawing.Point(1298, 12);
+            this.TxQNo.Name = "TxQNo";
+            this.TxQNo.Size = new System.Drawing.Size(85, 23);
+            this.TxQNo.TabIndex = 0;
+            this.TxQNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // TxRRg
+            // 
+            this.TxRRg.Location = new System.Drawing.Point(1298, 42);
+            this.TxRRg.Name = "TxRRg";
+            this.TxRRg.Size = new System.Drawing.Size(85, 23);
+            this.TxRRg.TabIndex = 1;
+            this.TxRRg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1218, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "數據數量";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1218, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "數據範圍";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(1220, 87);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(163, 38);
+            this.btnRandom.TabIndex = 4;
+            this.btnRandom.Text = "隨機數產生";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnBubble
+            // 
+            this.btnBubble.Location = new System.Drawing.Point(1221, 142);
+            this.btnBubble.Name = "btnBubble";
+            this.btnBubble.Size = new System.Drawing.Size(163, 38);
+            this.btnBubble.TabIndex = 5;
+            this.btnBubble.Text = "氣泡排序法";
+            this.btnBubble.UseVisualStyleBackColor = true;
+            this.btnBubble.Click += new System.EventHandler(this.btnBubble_Click);
+            // 
+            // btnTree
+            // 
+            this.btnTree.Location = new System.Drawing.Point(1221, 527);
+            this.btnTree.Name = "btnTree";
+            this.btnTree.Size = new System.Drawing.Size(163, 38);
+            this.btnTree.TabIndex = 6;
+            this.btnTree.Text = "堆積樹排序法";
+            this.btnTree.UseVisualStyleBackColor = true;
+            this.btnTree.Click += new System.EventHandler(this.btnTree_Click);
+            // 
+            // btnRadix
+            // 
+            this.btnRadix.Location = new System.Drawing.Point(1218, 472);
+            this.btnRadix.Name = "btnRadix";
+            this.btnRadix.Size = new System.Drawing.Size(163, 38);
+            this.btnRadix.TabIndex = 7;
+            this.btnRadix.Text = "基數排序法";
+            this.btnRadix.UseVisualStyleBackColor = true;
+            this.btnRadix.Click += new System.EventHandler(this.btnRadix_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(1218, 417);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(163, 38);
+            this.btnMerge.TabIndex = 8;
+            this.btnMerge.Text = "合併排序法";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnQuick
+            // 
+            this.btnQuick.Location = new System.Drawing.Point(1220, 362);
+            this.btnQuick.Name = "btnQuick";
+            this.btnQuick.Size = new System.Drawing.Size(163, 38);
+            this.btnQuick.TabIndex = 9;
+            this.btnQuick.Text = "快速排序法";
+            this.btnQuick.UseVisualStyleBackColor = true;
+            this.btnQuick.Click += new System.EventHandler(this.btnQuick_Click);
+            // 
+            // btnShell
+            // 
+            this.btnShell.Location = new System.Drawing.Point(1220, 307);
+            this.btnShell.Name = "btnShell";
+            this.btnShell.Size = new System.Drawing.Size(163, 38);
+            this.btnShell.TabIndex = 10;
+            this.btnShell.Text = "謝爾排序法";
+            this.btnShell.UseVisualStyleBackColor = true;
+            this.btnShell.Click += new System.EventHandler(this.btnShell_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(1220, 252);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(163, 38);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "插入排序法";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(1220, 197);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(163, 38);
+            this.btnSelect.TabIndex = 12;
+            this.btnSelect.Text = "選擇排序法";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(13, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Number";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1196, 614);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1221, 582);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 14);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "耗時 : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1274, 582);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 14);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1395, 641);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnShell);
+            this.Controls.Add(this.btnQuick);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnRadix);
+            this.Controls.Add(this.btnTree);
+            this.Controls.Add(this.btnBubble);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxRRg);
+            this.Controls.Add(this.TxQNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxQNo;
+        private System.Windows.Forms.TextBox TxRRg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnBubble;
+        private System.Windows.Forms.Button btnTree;
+        private System.Windows.Forms.Button btnRadix;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Button btnQuick;
+        private System.Windows.Forms.Button btnShell;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SortingExp_排序範例/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SortingExp_排序範例
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch watch = new Stopwatch();
+        int[] RandomArray = new int[10];
+        int Temp, QNo, RRg,Count01, Count02, Count03;
+        long TTime;
+        public Form1()
+        {
+            InitializeComponent();
+            ButtonOff();
+            label4.Text = "";
+        }
+        private void ButtonOff()
+        {
+            btnBubble.Enabled = false;
+            btnInsert.Enabled = false;
+            btnMerge.Enabled = false;
+            btnQuick.Enabled = false;
+            btnRadix.Enabled = false;
+            btnSelect.Enabled = false;
+            btnShell.Enabled = false;
+            btnTree.Enabled = false;
+        }
+        private void ButtonOn()
+        {
+            btnBubble.Enabled = true;
+            btnInsert.Enabled = true;
+            btnMerge.Enabled = true;
+            btnQuick.Enabled = true;
+            btnRadix.Enabled = true;
+            btnSelect.Enabled = true;
+            btnShell.Enabled = true;
+            btnTree.Enabled = true;
+        }
+        private void ShowChart()
+        {
+            chart1.Series["Number"].Points.Clear();
+            for (Count03 = 0; Count03 < RandomArray.Length; Count03++)
+            {
+                chart1.Series["Number"].Points.AddY(RandomArray[Count03]);
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 0x20) e.KeyChar = (char)0;                                 //禁止空格键(ASC II 0x20=space),如果為空格鍵則其值為(char)0((char)0為空值)
+            if ((e.KeyChar == 0x2D) && (((TextBox)sender).Text.Length == 0)) return;    //輸入為負(ASC II 0x2D=-)且整個textbox的總長度為0(在最前面)
+            if (e.KeyChar > 0x20)
+            {
+                try
+                {
+                    double.Parse(((TextBox)sender).Text + e.KeyChar.ToString());
+                }
+                catch
+                {
+                    e.KeyChar = (char)0;                                                //處理非法字符
+                }
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 0x20) e.KeyChar = (char)0;                                 //禁止空格键(ASC II 0x20=space),如果為空格鍵則其值為(char)0((char)0為空值)
+            if ((e.KeyChar == 0x2D) && (((TextBox)sender).Text.Length == 0)) return;    //輸入為負(ASC II 0x2D=-)且整個textbox的總長度為0(在最前面)
+            if (e.KeyChar > 0x20)
+            {
+                try
+                {
+                    double.Parse(((TextBox)sender).Text + e.KeyChar.ToString());
+                }
+                catch
+                {
+                    e.KeyChar = (char)0;                                                //處理非法字符
+                }
+            }
+        }
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            try
+            {
+                QNo = Convert.ToInt16(TxQNo.Text);
+            }
+            catch
+            {
+                MessageBox.Show("數據數量超出範圍");
+                TxQNo.Clear();
+                TxQNo.AppendText("32767");
+                QNo = 32767;
+            }
+            try
+            {
+                RRg = Convert.ToInt16(TxRRg.Text);
+            }
+            catch
+            {
+                MessageBox.Show("數據範圍超出");
+                TxRRg.Clear();
+                TxRRg.AppendText("32767");                
+                RRg = 32767;
+            }
+            finally
+            {
+                Random RND = new Random(Guid.NewGuid().GetHashCode());
+                Array.Resize(ref RandomArray, QNo);
+                for (Count01 = 0; Count01 < RandomArray.Length; Count01++)
+                {
+                    RandomArray[Count01] = RND.Next(1, RRg);
+                }
+                ButtonOn();
+                ShowChart();
+                btnRandom.Enabled = false;              
+                CtOff();
+            }
+        }
+        private void CtOff()
+        {
+            watch.Stop();
+            TTime += watch.ElapsedMilliseconds;
+            label4.Text = Convert.ToString(TTime) + "ms";
+            TTime = 0;
+        }
+        private void CtOn()
+        {
+            watch.Reset();
+            watch.Start();
+        }
+        private void btnBubble_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            for (Count01 = RandomArray.Length - 1; Count01 > 0; Count01--)     //掃瞄次數
+            {
+                for (Count02 = 0; Count02 < Count01; Count02++)     //比較、交換次數
+                {
+                    // 比較相鄰兩數，如第一數較大則交換
+                    if (RandomArray[Count02] > RandomArray[Count02 + 1])
+                    {
+                        Temp = RandomArray[Count02];
+                        RandomArray[Count02] = RandomArray[Count02 + 1];
+                        RandomArray[Count02 + 1] = Temp;                       
+                    }
+                }
+            }
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            for (Count01 = 0; Count01 < RandomArray.Length-1 ; Count01++)         //掃描5次
+            {
+                for (Count02 = Count01 + 1; Count02 < RandomArray.Length; Count02++)  //由i+1比較起，比較5次
+                {
+                    if (RandomArray[Count01] > RandomArray[Count02])  //比較第i及第j個元素
+                    {
+                        Temp = RandomArray[Count01];
+                        RandomArray[Count01] = RandomArray[Count02];
+                        RandomArray[Count02] = Temp;
+                    }
+                }    
+            }
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            for (Count01 = 1; Count01 < RandomArray.Length; Count01++)  //掃描迴圈次數為SIZE-1
+            {
+                Temp = RandomArray[Count01];
+                Count02 = Count01 - 1;
+                while (Count02 >= 0 && Temp < RandomArray[Count02])  //如果第二元素小於第一元素
+                {
+                    RandomArray[Count02 + 1] = RandomArray[Count02]; //就把所有元素往後推一個位置
+                    Count02--;
+                }
+                RandomArray[Count02 + 1] = Temp;       //最小的元素放到第一個元素               
+            }
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnShell_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            int jmp = RandomArray.Length / 2;
+            while (jmp != 0)
+            {
+                for (Count01 = jmp; Count01 < RandomArray.Length; Count01++)
+                {
+                    Temp = RandomArray[Count01];
+                    Count02 = Count01 - jmp;
+                    while (Count02 >= 0 && Temp < RandomArray[Count02])
+                    //插入排序法
+                    {
+                        RandomArray[Count02 + jmp] = RandomArray[Count02];
+                        Count02 = Count02 - jmp;
+                    }
+                    RandomArray[jmp + Count02] = Temp;
+                }
+                jmp = jmp / 2; //控制迴圈數
+            }
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnQuick_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            QuickSort(RandomArray, 0, RandomArray.Length-1);
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            mergeSort(RandomArray, 0, RandomArray.Length-1);
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnRadix_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            int   k;
+            for (Count01  = 1; Count01 <= 32767; Count01 = Count01 * 10)//n為基數，由個位數開始排序
+            {
+                //設定暫存陣列，[0~9位數][資料個數]，所有內容均為0
+                int[,] tmp = new int[10, 32767];
+                for (Count03 = 0; Count03 < RandomArray.Length; Count03++) //比對所有資料
+                {
+                    Count02 = (RandomArray[Count03] / Count01) % 10; //m為n位數的值，如36取十位數(36/10)%10=3 
+                    tmp[Count02, Count03] = RandomArray[Count03]; //把data[i]的值暫存於tmp裡
+                }
+
+                k = 0;
+                for (Count03 = 0; Count03 < 10; Count03++)
+                {
+                    for (Count02 = 0; Count02 < RandomArray.Length; Count02++)
+                    {
+                        if (tmp[Count03, Count02] != 0) //因一開始設定tmp={0}，故不為0者即為
+                        {
+                            //data暫存在tmp 裡的值，把tmp裡的值放回data[ ]裡
+                            RandomArray[k] = tmp[Count03, Count02];
+                            k++;
+                        }
+                    }
+                }
+            }
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        private void btnTree_Click(object sender, EventArgs e)
+        {
+            CtOn();
+            Heap(RandomArray, RandomArray.Length);
+            ShowChart();
+            CtOff();
+            btnRandom.Enabled = true;
+            ButtonOff();
+        }
+        #region 快速排序法I
+        //private static void QuickSort(int[] a, int low, int high)
+        //{
+        //    if (low >= high)
+        //    {
+        //        return;
+        //    }
+        //    int pivot = QuickSortOnce(a, low, high);//輸出每一次排序。
+
+        //    //對樞軸左端進行排序。
+        //    QuickSort(a, low, pivot - 1);
+        //    //對樞軸右端進行排序。
+        //    QuickSort(a, pivot + 1, high);
+        //}
+        //private static int QuickSortOnce(int[] a, int low, int high)
+        //{
+        //    //將首個元素作為樞軸。
+        //    int pivot = a[low];
+        //    int i = low, j = high;
+        //    while (i < j)
+        //    {
+        //        //從右往左，尋找首個小於povit的元素。
+        //        while (a[j] >= pivot && i < j)
+        //        {
+        //            j--;
+        //        }
+        //        //執行到此，j一定指向從右端起首個小於或等於povit的元素。執行替換。
+        //        a[i] = a[j];
+        //        //從左往右，尋找首個大於povit的元素。
+        //        while (a[i] <= pivot && i < j)
+        //        {
+        //            i++;
+        //        }
+        //        ////執行到此，j一定指向從右端起首個大於或等於povit的元素。執行替換。
+        //        a[j] = a[i];
+        //    }
+        //    //退出while迴圈，執行至此，必定是i==j的情況。i（或j）指向的既是樞軸的位置，定位該趟排序的樞軸並將該位置返回。
+        //    a[i] = pivot;
+        //    return i;
+        //}
+        #endregion
+        #region 快速排序法II
+        static void QuickSort(int[] array, int left, int right)
+        {
+            int temp;
+            if (left < right)
+            {
+                int i = left;
+                int j = right + 1;
+                while (true)
+                {
+                    while (i + 1 < array.Length && array[++i] < array[left]) ;
+                    while (j - 1 > -1 && array[--j] > array[left]) ;
+                    if (i >= j)
+                        break;
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                    //Swap(array, i, j);
+                }
+                temp = array[left];
+                array[left] = array[j];
+                array[j] = temp;
+                //Swap(array, left, j);
+                QuickSort(array, left, j - 1);
+                QuickSort(array, j + 1, right);
+            }
+        }
+        #endregion
+        #region 合併排序法
+        public static void mergeSort(int[] list, int left, int right)
+        {
+            int mid;
+            if (left < right)
+            {
+                mid = (left + right) / 2;
+                mergeSort(list, left, mid);//排序左半邊
+                mergeSort(list, mid + 1, right);//排序右半邊
+                merge(list, left, mid, right);//組合
+            }
+        }
+        public static void merge(int[] list, int left, int mid, int right)
+        {
+            int[] temp = new int[right - left + 1];//設定暫存陣列長度
+            int left1 = left, left2 = mid + 1, index = 0;//設定左邊極限
+            while ((left1 <= mid) && (left2 <= right))
+            {
+                if (list[left1] < list[left2])
+                {
+                    temp[index] = list[left1];
+                    left1++;
+                    index++;
+                }
+                else
+                {
+                    temp[index] = list[left2];
+                    left2++;
+                    index++;
+                }
+            }
+            while (left1 <= mid)
+            {
+                temp[index] = list[left1];
+                left1++;
+                index++;
+            }
+            while (left2 <= right)
+            {
+                temp[index] = list[left2];
+                left2++;
+                index++;
+            }
+            for (int i = 0; i < right - left + 1; i++)
+                list[left + i] = temp[i];
+        }
+        #endregion
+        #region 堆積樹排序法
+        public static void Heap(int[] data, int size)
+        {
+
+            int i, tmp;
+            for (i = (size / 2); i > 0; i--) //建立堆積樹節點	
+                Ad_heap(data, i, size - 1);           
+            for (i = size - 2; i > 0; i--) //堆積排序
+            {
+                tmp = data[i + 1];        //頭尾節點交換
+                data[i + 1] = data[1];
+                data[1] = tmp;
+                Ad_heap(data, 1, i);      //處理剩餘節點               
+            }
+        }
+        public static void Ad_heap(int[] data, int i, int size)
+        {
+            int j, tmp, post;
+            j = 2 * i;
+            tmp = data[i];
+            post = 0;
+            while (j <= size && post == 0)
+            {
+                if (j < size)
+                {
+                    if (data[j] < data[j + 1]) //找出最大節點
+                        j++;
+                }
+                if (tmp >= data[j]) //若樹根較大，結束比較過程
+                    post = 1;
+                else
+                {
+                    data[j / 2] = data[j];//若樹根較小，則繼續比較
+                    j = 2 * j;
+                }
+            }
+            data[j / 2] = tmp;      //指定樹根為父節點
+        }
+        #endregion
+    }
+}
